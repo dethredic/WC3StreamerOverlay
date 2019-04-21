f9bdf66325d13ca95fa637fb78bf76634b97a651
Niko <nikoebener@gmail.com>
2019-04-21 16:17:15 +0200

    warcraft3.info new json format (#11)
    * warcraft3.info new json format
    
    * changed to net for release

--- src/wc3info_alias_interface.py
+++ src/wc3info_alias_interface.py
@@ -8,13 +8,12 @@ class Wc3InfoAliasInterface:
     for entry in data:
       if entry['aka'] is None or entry['aka'] is '':
         continue
-      if entry['daily_history']['name'] == player.name:
-        alias_pat = re.compile(r'.*>(.*)</a>')
-        alias = alias_pat.match(entry['aka']).group(1)
+      if entry['aka']['aka'] == player.name:
+        alias = entry['aka']['player']['name']
     player.alias = alias
 
   def get_alias(player, gateway):
     url = 'https://warcraft3.info/stats/bnet_data?server=' + gateway
     response = requests.get(url)
     if response.status_code == requests.codes.ok:
-      Wc3InfoAliasInterface.__get_alias(player, response.json()['data'])
+      Wc3InfoAliasInterface.__get_alias(player, response.json()['ranking'])
